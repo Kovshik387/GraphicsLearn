1f94567e3be0301da0ca6211cbc019fbcdc627bf
Никита Фантокин <Fantokin03@mail.ru>
2023-04-12 01:13:22 +0300

    Add Task6

--- GraphicsLearn/Task_6/Task6.Designer.cs
+++ GraphicsLearn/Task_6/Task6.Designer.cs
@@ -1,6 +1,6 @@
 ﻿extern alias global1;
 using global1::Tao.Platform.Windows;
-
+using Tao.DevIl;
 namespace GraphicsLearn.Task_6
 {
     partial class Task6
@@ -33,11 +33,16 @@ namespace GraphicsLearn.Task_6
         {
             this.View3d = new global1::Tao.Platform.Windows.SimpleOpenGlControl();
             this.ShowView = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ShowFill = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // View3d
             // 
             this.View3d.AccumBits = ((byte)(0));
+            this.View3d.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.View3d.AutoCheckErrors = false;
             this.View3d.AutoFinish = false;
             this.View3d.AutoMakeCurrent = true;
@@ -47,25 +52,64 @@ namespace GraphicsLearn.Task_6
             this.View3d.DepthBits = ((byte)(16));
             this.View3d.Location = new System.Drawing.Point(12, 12);
             this.View3d.Name = "View3d";
-            this.View3d.Size = new System.Drawing.Size(562, 426);
+            this.View3d.Size = new System.Drawing.Size(951, 585);
             this.View3d.StencilBits = ((byte)(0));
             this.View3d.TabIndex = 0;
+            this.View3d.Load += new System.EventHandler(this.View3d_Load);
             // 
             // ShowView
             // 
-            this.ShowView.Location = new System.Drawing.Point(580, 415);
+            this.ShowView.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShowView.Location = new System.Drawing.Point(969, 545);
             this.ShowView.Name = "ShowView";
-            this.ShowView.Size = new System.Drawing.Size(208, 23);
+            this.ShowView.Size = new System.Drawing.Size(174, 23);
             this.ShowView.TabIndex = 1;
             this.ShowView.Text = "button1";
             this.ShowView.UseVisualStyleBackColor = true;
             this.ShowView.Click += new System.EventHandler(this.ShowView_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Конус",
+            "Куб",
+            "Цилиндр",
+            "Додекаэдр",
+            "Икосаэдр",
+            "Октаэдр",
+            "Ромбический додекаэдр",
+            "Фрактал Губка Серпинского",
+            "Сфера",
+            "Чайник",
+            "Тетраэдр",
+            "Тор"});
+            this.comboBox1.Location = new System.Drawing.Point(969, 518);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(174, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // ShowFill
+            // 
+            this.ShowFill.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShowFill.Location = new System.Drawing.Point(969, 574);
+            this.ShowFill.Name = "ShowFill";
+            this.ShowFill.Size = new System.Drawing.Size(174, 23);
+            this.ShowFill.TabIndex = 3;
+            this.ShowFill.Text = "button2";
+            this.ShowFill.UseVisualStyleBackColor = true;
+            this.ShowFill.Click += new System.EventHandler(this.ShowFill_Click);
+            // 
             // Task6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1155, 609);
+            this.Controls.Add(this.ShowFill);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.ShowView);
             this.Controls.Add(this.View3d);
             this.Name = "Task6";
@@ -78,5 +122,7 @@ namespace GraphicsLearn.Task_6
 
         private SimpleOpenGlControl View3d;
         private System.Windows.Forms.Button ShowView;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button ShowFill;
     }
 }
--- GraphicsLearn/Task_6/Task6.cs
+++ GraphicsLearn/Task_6/Task6.cs
@@ -11,46 +11,239 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Tao.FreeGlut;
 using Tao.OpenGl;
-
+using Tao.DevIl;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace GraphicsLearn.Task_6
 {
     public partial class Task6 : Form
     {
+        enum Primitives
+        {
+            None = 0,
+            Cone,
+            Cube,
+            Cylinder,
+            Dodecahedron,
+            Icosagedron,
+            Octahedron,
+            RhombicDodecahedron,
+            Sponge,
+            Sphere,
+            Teapot,
+            Tetrahedron,
+            Torus
+        };
+
         public Task6()
         {
             InitializeComponent();
             View3d.InitializeContexts();
 
-            Glut.glutInit();
-            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
 
-            Gl.glClearColor(255, 255, 255, 1);
+        }
 
-            Gl.glViewport(0, 0, View3d.Width, View3d.Height);
+        public void Display()
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
 
             Gl.glMatrixMode(Gl.GL_PROJECTION);
-            Gl.glLoadIdentity();
-            Glu.gluPerspective(45, (float)View3d.Width / (float)View3d.Height, 0.1, 200);
-
+            Gl.glRotated(-30, 1, 0, 0);
+            Gl.glRotated(45, 0, 1, 0);
             Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Glu.gluPerspective(45, (float)View3d.Width / (float)View3d.Height, 0.1, 200);
             Gl.glLoadIdentity();
-            Gl.glEnable(Gl.GL_DEPTH_TEST);
+        }
+
+        private void SelectFillPrimitive()
+        {
+            var currentPrimitive = (Primitives)(this.comboBox1.SelectedIndex + 1);
+
+            switch (currentPrimitive)
+            {
+                case Primitives.None:
+                    return;
+                case Primitives.Cone:
+                    Glut.glutSolidCone(0.2, 0.75, 16, 8);
+                    break;
+                case Primitives.Cube:
+                    Glut.glutSolidCube(0.75);
+                    break;
+                case Primitives.Cylinder:
+                    Glut.glutSolidCylinder(0.2, 0.75, 16, 16);
+                    break;
+                case Primitives.Dodecahedron:
+                    Gl.glTranslated(3, 2, -3);
+                    Glut.glutSolidDodecahedron();
+                    break;
+                case Primitives.Icosagedron:
+                    Glut.glutSolidIcosahedron();
+                    break;
+                case Primitives.Octahedron:
+                    Glut.glutSolidOctahedron();
+                    break;
+                case Primitives.RhombicDodecahedron:
+                    Glut.glutSolidRhombicDodecahedron();
+                    break;
+                case Primitives.Sponge:
+                    Glut.glutSolidSierpinskiSponge(7, new double[] { 0, 0 }, 1);
+                    break;
+                case Primitives.Sphere:
+                    Glut.glutSolidSphere(0.75, 16, 16);
+                    break;
+                case Primitives.Teapot:
+                    Glut.glutSolidTeapot(0.5);
+                    break;
+                case Primitives.Tetrahedron:
+                    Glut.glutSolidTetrahedron();
+                    break;
+                case Primitives.Torus:
+                    Glut.glutSolidTorus(0.15, 0.65, 16, 16);
+                    break;
+            }
+        }
+
+        private void SelectWirePrimivite()
+        {
+            var currentPrimitive = (Primitives)(this.comboBox1.SelectedIndex + 1);
+
+            switch (currentPrimitive)
+            {
+                case Primitives.None:
+                    return;
+                case Primitives.Cone:
+                    Glut.glutWireCone(0.2, 0.75, 16, 8);
+                    break;
+                case Primitives.Cube:
+                    Glut.glutWireCube(0.75);
+                    break;
+                case Primitives.Cylinder:
+                    Glut.glutWireCube(0.75);
+                    break;
+                case Primitives.Dodecahedron:
+                    Gl.glTranslated(3, 2, -3);
+                    Glut.glutWireDodecahedron();
+                    break;
+                case Primitives.Icosagedron:
+                    Glut.glutWireIcosahedron();
+                    break;
+                case Primitives.Octahedron:
+                    Glut.glutWireOctahedron();
+                    break;
+                case Primitives.RhombicDodecahedron:
+                    Glut.glutWireRhombicDodecahedron();
+                    break;
+                case Primitives.Sponge:
+                    Glut.glutWireSierpinskiSponge(7, new double[] { 0, 0, 0 }, 1);
+                    break;
+                case Primitives.Sphere:
+                    Glut.glutWireSphere(0.75, 16, 16);
+                    break;
+                case Primitives.Teapot:
+                    Glut.glutWireTeapot(0.5);
+                    break;
+                case Primitives.Tetrahedron:
+                    Glut.glutWireTetrahedron();
+                    break;
+                case Primitives.Torus:
+                    Glut.glutWireTorus(0.15, 0.65, 16, 16);
+                    break;
+            }
         }
 
         private void ShowView_Click(object sender, EventArgs e)
         {
-            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
-            Gl.glLoadIdentity();
-            Gl.glColor3f(1f,0,4f);
+            Display();
+            Gl.glViewport(0, 0, View3d.Width, View3d.Height);
+            var random = new Random();
+
+            Gl.glColor3d(random.NextDouble(), random.NextDouble(), random.NextDouble());
             Gl.glPushMatrix();
-            Gl.glTranslated(0, 0, -3);
-            Gl.glRotated(-30, 1, 0, 0);
-            Gl.glRotated(45, 0, 1, 0);
-            Glut.glutSolidSierpinskiSponge(7,new double[] {0,0,0} ,1);
+
+            SelectWirePrimivite();
+
             Gl.glPopMatrix();
             Gl.glFlush();
             View3d.Invalidate();
         }
+
+        private void ShowFill_Click(object sender, EventArgs e)
+        {
+            Display();
+
+            Gl.glEnable(Gl.GL_TEXTURE_2D);
+            string path = @"C:\Users\Yrulewet\source\repos\LearcnCS\GraphicsLearn\GraphicsLearn\Task_6\Textures\test.jpg";
+
+            int index;
+            uint mGlTextureObject = 0;
+
+            
+            Il.ilGenImages(1, out index);
+            Il.ilBindImage(index);
+
+            bool suc = Il.ilLoadImage(path);
+
+            if (suc)
+            {
+                int width = Il.ilGetInteger(Il.IL_IMAGE_WIDTH);
+                int height = Il.ilGetInteger(Il.IL_IMAGE_HEIGHT);
+                // Число бит на пиксель
+                int bitspp = Il.ilGetInteger(Il.IL_IMAGE_BITS_PER_PIXEL);
+                switch (bitspp)
+                {
+                    // Создаем текстуру, используя GL_RGB или GL_RGBA
+                    case 24:
+                        mGlTextureObject = MakeGlTexture(Gl.GL_RGB, Il.ilGetData(), width, height); break;
+                    case 32:
+                        mGlTextureObject = MakeGlTexture(Gl.GL_RGBA, Il.ilGetData(), width, height); break;
+                }
+                Il.ilDeleteImage(index);
+            }
+
+            Gl.glEnable(Gl.GL_TEXTURE_GEN_S);
+            Gl.glEnable(Gl.GL_TEXTURE_GEN_T);
+
+            Gl.glTexGeni(Gl.GL_S, Gl.GL_TEXTURE_GEN_MODE, Gl.GL_OBJECT_LINEAR);
+            Gl.glTexGeni(Gl.GL_T, Gl.GL_TEXTURE_GEN_MODE, Gl.GL_OBJECT_LINEAR);
+            
+            SelectFillPrimitive();
+            Gl.glDisable(Gl.GL_TEXTURE_GEN_S);
+            Gl.glDisable(Gl.GL_TEXTURE_GEN_T);
+            Gl.glDisable(Gl.GL_TEXTURE_2D);
+            Gl.glDeleteTextures(1, ref mGlTextureObject);
+
+            View3d.Invalidate();
+        }
+        private uint MakeGlTexture(int Format, IntPtr pixels, int w, int h)
+        {
+            // Идентификатор текстуры
+            uint texObject;
+            // Генерируем под номером 1 текстуру
+            Gl.glGenTextures(1, out texObject);
+            // Выполняем привязка OpenGL к созданной текстуре
+            Gl.glBindTexture(Gl.GL_TEXTURE_2D, texObject);
+            // Режимы фильтрации текстуры
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
+            // Замещаем существующую заливку
+            Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_REPLACE);
+            // Создаем RGB или RGBA текстуру (в зависимости от значения параметра Format)
+            Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Format, w, h, 0, Format, Gl.GL_UNSIGNED_BYTE, pixels);
+            // Возвращаем идентификатор текстуры
+            return texObject;
+        }
+
+        private void View3d_Load(object sender, EventArgs e)
+        {
+            Gl.glClearColor(255, 255, 255, 1);
+            Il.ilInit();
+            Il.ilEnable(Il.IL_ORIGIN_SET);
+
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            
+        }
     }
+
 }
